85f185292e2db9f8c9929dbebf685063dee5cb3a
Sangram <okclsangram@gmail.com>
2026-07-27 13:17:24 +0530

    feat: add site favicon and apple-touch-icon to document head

--- index.php
+++ index.php
@@ -9,6 +9,8 @@ $pageTitle = 'AIS Web';
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?= htmlspecialchars($pageTitle, ENT_QUOTES, 'UTF-8') ?></title>
+    <link rel="icon" type="image/png" href="assets/logo/logo.png">
+    <link rel="apple-touch-icon" href="assets/logo/logo.png">
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
